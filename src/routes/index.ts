9cf1f7af736a9685b3d6e8f9916a7f80c44e0521
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-04-07 19:22:15 +0200

    use the "cloudRoutes" router to handle the request

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,10 @@
-import userRoutes from './user.routes';
-import { Router } from 'express';
+import userRoutes from "./user.routes";
+import { Router } from "express";
+import cloudRoutes from "./cloud.routes";
 
 const router = Router();
 
-router.use('/users', userRoutes);
-
-
-
+router.use("/users", userRoutes);
+router.use("/cloud", cloudRoutes);
 
 export default router;
-
-
-
-
-
-
