d1fd1b11619a4aeafccdec11ca26727d3cb4dc25
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-04-07 19:04:36 +0200

    update chat model

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,34 +1,30 @@
-import { Schema, Document, model} from "mongoose";
-
-
+import { Schema, Document, model } from "mongoose";
 
 // Define schema for Chat model
- interface ChatDoc extends Document {
-    members: Schema.Types.ObjectId[];
-    pinned: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+interface ChatDoc extends Document {
+  members: Schema.Types.ObjectId[];
+  pinned: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const ChatSchema: Schema = new Schema(
-    {
-        members: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User",
-                maxlength: 2,
-            },
-        ],
-        pinned: {
-            type: Boolean,
-            default: false,
-        },
-
+  {
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        maxlength: 2,
+      },
+    ],
+    pinned: {
+      type: Boolean,
+      default: false,
     },
-    { timestamps: true }
+  },
+  { timestamps: true }
 );
 
 const ChatModel = model<ChatDoc>("Chat", ChatSchema);
 
-export { ChatModel,  }
-
+export { ChatModel };
